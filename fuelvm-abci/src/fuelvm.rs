8230a9ea44307af44576e833b57908977309a7ee
Diego <31937514+Ferret-san@users.noreply.github.com>
2022-11-18 11:15:42 -0500

    Implement `deliver_tx`, `commit`,  and `end_block`

--- fuelvm-abci/src/fuelvm.rs
+++ fuelvm-abci/src/fuelvm.rs
@@ -1,31 +1,250 @@
 //! FuelVM as an ABCI application
 use std::{
-    collections::HashMap,
     future::Future,
     pin::Pin,
+    sync::Arc,
     task::{Context, Poll},
 };
+
+use tokio::sync::Mutex;
 
 use bytes::Bytes;
 use futures::future::FutureExt;
 use structopt::StructOpt;
 use tower::{Service, ServiceBuilder};
 
-use tendermint::abci::{response, Event, EventAttributeIndexExt, Request, Response};
+use tendermint::{
+    abci::{
+        request::{EndBlock, Query, Request},
+        response, Response,
+    },
+    block::Height,
+};
 
 use tower_abci::{split, BoxError, Server};
 
-use fuel_vm::{memory_client::MemoryClient, storage::MemoryStorage, transactor};
+use fuel_vm::{
+    interpreter::CheckedMetadata,
+    prelude::{ExecutableTransaction, Interpreter},
+    state::ProgramState,
+};
 
-/// Application State
+use fuel_vm::{prelude::Word, storage::MemoryStorage};
+
+use fuel_core_interfaces::{common::state::StateTransition, executor::Error as ExecutionError};
+
+use fuel_tx::{Checked, CheckedTransaction, IntoChecked, Transaction};
+
+use fuel_core::chain_config::ChainConfig;
+
+/// The app's state, containing a FuelVM
 #[derive(Clone, Debug)]
-pub struct State<Client> {
-    pub block_height: u32,
+pub struct State<Db> {
+    pub block_height: i64,
     pub app_hash: Vec<u8>,
-    pub client: Client,
+    pub db: Db,
+    pub config: ChainConfig,
 }
 
-impl Service<Request> for State<MemoryClient> {
+/// uses rocksdb when rocksdb features are enabled
+/// uses in-memory when rocksdb features are disabled
+impl Default for State<MemoryStorage> {
+    fn default() -> Self {
+        Self {
+            block_height: 0,
+            app_hash: Vec::new(),
+            db: MemoryStorage::default(),
+            config: ChainConfig::default(),
+        }
+    }
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+pub struct TransactionResult {
+    transaction: Transaction,
+    gas: u64,
+    result: Option<ProgramState>,
+}
+
+impl State<MemoryStorage> {
+    fn into_checked_basic(&self, tx: Transaction) -> eyre::Result<CheckedTransaction> {
+        let checked_tx: CheckedTransaction = tx
+            .clone()
+            .into_checked_basic(
+                self.block_height as Word,
+                &self.config.transaction_parameters,
+            )?
+            .into();
+
+        Ok(checked_tx)
+    }
+
+    fn execute<Tx>(&mut self, checked_tx: Checked<Tx>) -> Result<TransactionResult, ExecutionError>
+    where
+        Tx: ExecutableTransaction + PartialEq,
+        <Tx as IntoChecked>::Metadata: CheckedMetadata + Clone,
+    {
+        let tx_id = checked_tx.transaction().id();
+
+        let mut vm = Interpreter::with_storage(&mut self.db, self.config.transaction_parameters);
+
+        let vm_result: StateTransition<_> = vm
+            .transact(checked_tx.clone())
+            .map_err(|error| ExecutionError::VmExecution {
+                error,
+                transaction_id: tx_id,
+            })?
+            .into();
+
+        if !vm_result.should_revert() {
+            self.db.commit();
+        }
+
+        self.db.persist();
+
+        Ok(TransactionResult {
+            transaction: vm_result.tx().clone().into(),
+            gas: vm_result.tx().price(),
+            result: Some(*vm_result.state()),
+        })
+    }
+}
+
+// The Application
+pub struct App<Db> {
+    pub committed_state: Arc<Mutex<State<Db>>>,
+    pub current_state: Arc<Mutex<State<Db>>>,
+}
+
+impl Default for App<MemoryStorage> {
+    fn default() -> Self {
+        Self {
+            committed_state: Arc::new(Mutex::new(State::default())),
+            current_state: Arc::new(Mutex::new(State::default())),
+        }
+    }
+}
+
+impl<Db: Clone> App<Db> {
+    pub fn new(state: State<Db>) -> Self {
+        let committed_state = Arc::new(Mutex::new(state.clone()));
+        let current_state = Arc::new(Mutex::new(state));
+
+        App {
+            committed_state,
+            current_state,
+        }
+    }
+}
+
+// TODO: Implement all ABCI methods
+impl App<MemoryStorage> {
+    async fn info(&self) -> response::Info {
+        let state = self.current_state.lock().await;
+
+        response::Info {
+            data: "fuelvm-abci".to_string(),
+            version: "0.1.0".to_string(),
+            app_version: 1,
+            last_block_height: Height::try_from(state.block_height).unwrap(),
+            last_block_app_hash: state.app_hash.to_vec().into(),
+        }
+    }
+
+    // Should I worry about coinbase transactions? fees? etc?
+    async fn deliver_tx(&mut self, deliver_tx_request: Bytes) -> response::DeliverTx {
+        tracing::trace!("delivering tx");
+        let mut state = self.current_state.lock().await;
+
+        let tx: Transaction = match serde_json::from_slice(&deliver_tx_request) {
+            Ok(tx) => tx,
+            Err(_) => {
+                tracing::error!("could not decode request");
+                return response::DeliverTx {
+                    data: "could not decode request".into(),
+                    ..Default::default()
+                };
+            }
+        };
+
+        let checked_tx = state.into_checked_basic(tx).unwrap();
+
+        let result: TransactionResult;
+
+        match checked_tx {
+            CheckedTransaction::Script(script) => result = state.execute(script).unwrap(),
+            CheckedTransaction::Create(create) => result = state.execute(create).unwrap(),
+            CheckedTransaction::Mint(_) => {
+                // Right now, we only support `Mint` transactions for coinbase,
+                // which are processed separately as a first transaction.
+                //
+                // All other `Mint` transaction is not allowed.
+                return response::DeliverTx {
+                    data: "transaction not support".into(),
+                    ..Default::default()
+                };
+            }
+        }
+
+        tracing::trace!("executed tx");
+
+        response::DeliverTx {
+            data: Bytes::from(serde_json::to_vec(&result).unwrap()),
+            ..Default::default()
+        }
+    }
+
+    async fn end_block(&mut self, end_block_request: EndBlock) -> response::EndBlock {
+        tracing::trace!("ending block");
+        let mut current_state = self.current_state.lock().await;
+        current_state.block_height = end_block_request.height;
+        current_state.app_hash = vec![];
+        tracing::trace!("done");
+
+        response::EndBlock::default()
+    }
+
+    async fn commit(&mut self) -> response::Commit {
+        tracing::trace!("taking lock");
+        let current_state = self.current_state.lock().await.clone();
+        let mut committed_state = self.committed_state.lock().await;
+        *committed_state = current_state;
+        tracing::trace!("committed");
+
+        response::Commit {
+            data: Bytes::from((*committed_state).app_hash.clone()),
+            retain_height: Height::from(0 as u32),
+        }
+    }
+
+    // Paradigm replicated the eth_call interface
+    // what's the Fuel equivalent?
+    // async fn query(&self, query_request: Query) -> response::Query {
+    //     let state = self.current_state.lock().await;
+
+    //     // TODO: Implement a type for the available queries
+    //     let query = match serde_json::from_slice(&query_request.data) {
+    //         Ok(tx) => tx,
+    //         // no-op just logger
+    //         Err(_) => {
+    //             return response::Query {
+    //                 value: "could not decode request".into(),
+    //                 ..Default::default()
+    //             };
+    //         }
+    //     };
+
+    //     // Use dry_run, return the response to the query
+
+    //     response::Query {
+    //         key: query_request.data,
+    //         // value
+    //         ..Default::default()
+    //     }
+    // }
+}
+
+impl Service<Request> for App<MemoryStorage> {
     type Response = Response;
     type Error = BoxError;
     type Future = Pin<Box<dyn Future<Output = Result<Response, BoxError>> + Send + 'static>>;
@@ -37,65 +256,42 @@ impl Service<Request> for State<MemoryClient> {
     fn call(&mut self, req: Request) -> Self::Future {
         tracing::info!(?req);
 
+        let runtime = tokio::runtime::Builder::new_multi_thread()
+            .worker_threads(1)
+            .enable_all()
+            .build()
+            .unwrap();
+
         let rsp = match req {
             // handled messages
-            Request::Info(_) => Response::Info(self.info()),
-            Request::Query(query) => Response::Query(self.query(query.data)),
-            Request::DeliverTx(deliver_tx) => Response::DeliverTx(self.deliver_tx(deliver_tx.tx)),
-            Request::Commit => Response::Commit(self.commit()),
+            Request::Info(_) => Response::Info(runtime.block_on(self.info())),
+            // Need to handle query
+            Request::Query(_) => Response::Query(Default::default()),
+            Request::DeliverTx(deliver_tx) => {
+                Response::DeliverTx(runtime.block_on(self.deliver_tx(deliver_tx.tx)))
+            }
+            Request::Commit => Response::Commit(runtime.block_on(self.commit())),
             // unhandled messages
             Request::Flush => Response::Flush,
             Request::Echo(_) => Response::Echo(Default::default()),
             Request::InitChain(_) => Response::InitChain(Default::default()),
             Request::BeginBlock(_) => Response::BeginBlock(Default::default()),
             Request::CheckTx(_) => Response::CheckTx(Default::default()),
-            Request::EndBlock(_) => Response::EndBlock(Default::default()),
+            Request::EndBlock(end_block) => {
+                Response::EndBlock(runtime.block_on(self.end_block(end_block)))
+            }
             Request::ListSnapshots => Response::ListSnapshots(Default::default()),
             Request::OfferSnapshot(_) => Response::OfferSnapshot(Default::default()),
             Request::LoadSnapshotChunk(_) => Response::LoadSnapshotChunk(Default::default()),
             Request::ApplySnapshotChunk(_) => Response::ApplySnapshotChunk(Default::default()),
+            Request::SetOption(_) => Response::SetOption(tendermint::abci::response::SetOption {
+                code: 0,
+                log: String::from("N/A"),
+                info: String::from("N/A"),
+            }),
         };
         tracing::info!(?rsp);
         async move { Ok(rsp) }.boxed()
-    }
-}
-
-impl Default for State<MemoryClient> {
-    fn default() -> Self {
-        Self {
-            block_height: 0,
-            app_hash: Vec::new(),
-            client: MemoryClient::from(MemoryStorage::default()),
-        }
-    }
-}
-
-// TODO: Implement all ABCI methods
-impl State<MemoryClient> {
-    fn info(&self) -> response::Info {
-        response::Info {
-            data: "fuelvm-abci".to_string(),
-            version: "0.1.0".to_string(),
-            app_version: 1,
-            last_block_height: self.block_height.into(),
-            last_block_app_hash: self.app_hash.to_vec().into(),
-        }
-    }
-
-    // Paradigm replicated the eth_call interface
-    // what's the Fuel equivalent?
-    fn query(&self, query: Bytes) -> response::Query {
-        // TODO
-    }
-
-    fn deliver_tx(&mut self, tx: Bytes) -> response::DeliverTx {
-        // TODO
-        // Call the `transact` method from `MemoryClient::transact`
-    }
-
-    fn commit(&mut self) -> response::Commit {
-        // TODO
-        // Call `MemoryClient::persist()`
     }
 }
 
@@ -116,7 +312,7 @@ async fn main() {
     let opt = Opt::from_args();
 
     // Construct our ABCI application.
-    let service = State::default();
+    let service = App::default();
 
     // Split it into components.
     let (consensus, mempool, snapshot, info) = split::service(service, 1);
